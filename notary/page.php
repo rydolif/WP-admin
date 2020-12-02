dfd55b63a748b3cd7d1082650da9f41fa59ad5d0
master <rudolifrudolif@gmail.com>
2020-12-02 19:55:53 +0200

    edits

--- notary/page.php
+++ notary/page.php
@@ -147,11 +147,11 @@
 								</a>
 								<div class="news__item_desc">
 									<h4 class="h4">
-										<a href="#">
+										<a href="<?php the_permalink(); ?>">
 											<?php the_title(); ?>
 										</a>
 									</h4>
-									<?php the_excerpt(); ?>
+									<p class="news__item_text"><?php echo wp_trim_words( get_the_content(), 15, '...' ); ?></p>
 									<div class="news__item_more">
 										<a href="<?php the_permalink(); ?>">
 											Читать далее 
